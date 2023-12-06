5cdd84fb12ec41baa1a1cbd7a6eab8240a947829
Cassio <cassiojr0108@gmail.com>
2023-12-06 14:56:13 -0300

    Create conection with dataBase and create migrations

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,8 +1,23 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { routes } from './routes'
+import { AppError } from '../errors'
 
 const app = express()
 app.use(express.json())
 app.use(routes)
 
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if(error instanceof AppError){
+        return res.status(error.statusCode).json({
+            status: 'error',
+            message: error.message
+        })
+    }
+
+    console.log(error)
+    return res.status(500).json({
+        status: 'error',
+        message: 'internal server error'
+    })
+} )
 export { app }
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,15 @@
 import 'dotenv/config'
 import { app } from './app'
+import { dataSource } from '../typeorm'
 
+dataSource.initialize().then(() => {
+    
+    app.listen(process.env.PORT,() => {
+        console.log(`Servidor aberto na porta ${process.env.PORT}`)
+    })
 
-app.listen(process.env.PORT,() => {
-    console.log(`Servidor aberto na porta ${process.env.PORT}`)
+}).catch((err) => {
+    if(err) {
+        console.log('Erro ao inicializar DataBase!!!')
+    }
 })
